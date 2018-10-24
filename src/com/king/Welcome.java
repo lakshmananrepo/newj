015ff56323dc15487ce34adb59d21496050fa3a8
'lakshmanan' <'lakshmanan@mnxw.org'>
2018-10-24 12:58:12 +0530

    Frame added

--- src/com/king/Welcome.java
+++ src/com/king/Welcome.java
@@ -1,4 +1,7 @@
 package com.king;
+
+import javax.swing.JFrame;
+
 /**
  *
  * @author Lakshmanan.0018
@@ -6,5 +9,11 @@ package com.king;
 public class Welcome {
     public static void main(String[] args) {
         System.out.println("Java new features....");
+        JFrame.setDefaultLookAndFeelDecorated(true);
+        JFrame frame = new JFrame("Testing....");
+        frame.setSize(400,400);
+        frame.setVisible(true);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
