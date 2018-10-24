c08abee7007f110b16eca8551b2842a55ae30057
'lakshmanan' <'lakshmanan@mnxw.org'>
2018-10-24 11:00:58 +0530

    Initial commit.

---
+++ src/com/king/Welcome.java
@@ -0,0 +1,10 @@
+package com.king;
+/**
+ *
+ * @author Lakshmanan.0018
+ */
+public class Welcome {
+    public static void main(String[] args) {
+        System.out.println("Java new features....");
+    }
+}
