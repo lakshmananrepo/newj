24d236a2242df545362906216c4f24261a2718ee
'lakshmanan' <'lakshmanan@mnxw.org'>
2018-10-24 15:16:47 +0530

    button was added to frame

--- src/com/king/Welcome.java
+++ src/com/king/Welcome.java
@@ -1,19 +1,39 @@
 package com.king;
 
+import java.awt.FlowLayout;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
 
 /**
  *
  * @author Lakshmanan.0018
  */
-public class Welcome {
+public class Welcome extends JFrame {
+    JButton btn;
+    public Welcome(){
+        setTitle("Exampel Frame");
+        btn = new JButton("Click");
+        setLayout(new FlowLayout());
+        add(btn);
+        
+        setSize(500,400);
+        setVisible(true);
+        
+        //setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e){
+                System.out.println("Window about to close...");
+                e.getWindow().dispose();
+            }
+        });
+        
+        btn.addActionListener(event-> System.out.println("btn was clicked..." ));
+    }
     public static void main(String[] args) {
-        System.out.println("Java new features....");
         JFrame.setDefaultLookAndFeelDecorated(true);
-        JFrame frame = new JFrame("Testing....");
-        frame.setSize(400,400);
-        frame.setVisible(true);
-        frame.setResizable(false);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        SwingUtilities.invokeLater(()->new Welcome());
     }
 }
