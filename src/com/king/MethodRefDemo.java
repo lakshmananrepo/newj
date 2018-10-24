7d72fb85d9c44c0f263118de31030e5ae05e9203
'lakshmanan' <'lakshmanan@mnxw.org'>
2018-10-24 16:30:55 +0530

    MethodRedDemo was added

---
+++ src/com/king/MethodRefDemo.java
@@ -0,0 +1,24 @@
+package com.king;
+
+import java.util.List;
+import java.util.Arrays;
+
+/**
+ *
+ * @author Lakshmanan.0018
+ * A method reference is the shorthand syntax for a lambda expression that executes just ONE method.
+ */
+public class MethodRefDemo {
+    public static void main(String[] args) {
+        List <Integer> list = Arrays.asList(10,20,30,40,50);
+     
+        //for(int num: list)print(num);        
+        //for(int i=0; i<list.size(); i++) print(list.get(i));        
+        //list.forEach(n->print(n));
+        
+        list.forEach(MethodRefDemo::print);
+    }
+    static void print(final int num){
+        System.out.println("I'm printing..." + num);
+    }
+}
