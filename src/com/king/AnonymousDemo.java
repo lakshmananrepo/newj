ebec1c7d8172a98f87d3ea9d742a2a1b294aa7ae
lakshmanan <lakshmanan@192.168.1.105>
2018-10-27 22:35:00 +0530

    welcome message adds

--- src/com/king/AnonymousDemo.java
+++ src/com/king/AnonymousDemo.java
@@ -2,6 +2,8 @@ package com.king;
 
 public class AnonymousDemo {    
     public static void main(String[] args) {
-        
+        System.out.println("starts...");
+        System.out.println("Hello, World!");
+        System.out.println("ends...");
     }    
 }
